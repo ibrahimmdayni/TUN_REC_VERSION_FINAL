118152c0b6f17143dfcadb21ae63f8e074cce827
ibrahim <ibrahim@SAMSUNG-PC>
2014-02-18 11:07:48 +0100

    creation de projet

---
+++ TunRec/src/tunrec/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tunrec;
+
+/**
+ *
+ * @author ibrahim
+ */
+public class Test {
+    private int id ;
+    
+    
+}
